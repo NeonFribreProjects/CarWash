f934f0438c21b67039ccc35eec2705796545f8cc
kevdj <kevindjoussa@gmail.com>
2025-06-29 01:35:53 +0100

    new migration

---
+++ CarWash/server/prisma/migrations/20250629000000_add_vehicle_fields/migration.sql
@@ -0,0 +1,6 @@
+﻿-- AddVehicleColumns
+ALTER TABLE "Service" ADD COLUMN "vehicleType" TEXT;
+ALTER TABLE "Booking" ADD COLUMN "vehicleMake" TEXT;
+ALTER TABLE "Booking" ADD COLUMN "vehicleModel" TEXT;
+ALTER TABLE "Booking" ADD COLUMN "vehicleColor" TEXT;
+ALTER TABLE "Booking" ADD COLUMN "licensePlate" TEXT;
